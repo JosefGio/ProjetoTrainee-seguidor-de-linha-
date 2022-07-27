5a32af9043e50d08b340a77f46a99c167dd547aa
JosefGio <108878524+JosefGio@users.noreply.github.com>
2022-07-27 14:28:15 -0300

    Update PWM .c

--- PWM .c
+++ PWM .c
@@ -18,10 +18,15 @@ void setDuty_2 (int duty) //MotorEsquerdo
     OCR18 = dut; //registrador de PWM 
 
 
+}#include "PWM.h"
+
+void PWM_init(void)
+{
+  TCCR1A = 0xA3; //Configura operacao em fast PWM, utilizando registradores OCR1x para comparacao
+
 }
 
-
-void setFreq (char option)
+void setup_pwm_setFreq(char option) 
 {
     /*
     TABLE:
@@ -34,9 +39,22 @@ void setFreq (char option)
           12     62,5    Hz
           13     15,6    Hz 
      */
-    TCCR1B = 12;
+    TCCR1B = option;
+	
+    /*
+    TABLE:
+  	//no phase corret PWM Mode
+        option  frequency (as frequências no timer 1 são menores do que as frequências nos timers 0 e 2)
+        
+          9       8     kHz
+          10       1     kHz
+          11     125      Hz
+          12     31,25    Hz
+          13     7,8      Hz
+     */
 }
-	void pwm_set_duty_service(unsigned int duty, unsigned char channel)
+
+void pwm_set_duty_service(unsigned int duty, unsigned char channel)
 {   
   if (channel == 1)
   {
@@ -45,6 +63,4 @@ void setFreq (char option)
   }
   OCR1B = duty; //registrador de PWM do OC1B
 }
-
-
 
