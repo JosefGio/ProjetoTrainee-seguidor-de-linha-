812b59118bd88587848988b6a0d37a7ce9f762b0
JosefGio <108878524+JosefGio@users.noreply.github.com>
2022-07-29 18:27:05 -0300

    Update sketch1.c

--- sketch1.c
+++ sketch1.c
@@ -6,21 +6,20 @@
 #include <util/delay.h>
 #include "PWM.h"
 
-#define set_bit(reg, bit) (reg|= (1 << bit))
+#define set_bit(reg, bit) (reg |= (1 << bit))
 #define clear_bit(reg, bit) (reg &= ~(1 << bit))
-#define test_bit(reg,bit)  (reg & (1<<bit)) 
+#define test_bit(reg, bit) (reg & (1 << bit))
 
-#define In1 PB5 //13
-#define In2 PB4 //12  
-#define In3 PD7 //7  
-#define In4 PD6 //6 
-#define ENA PB2 //10 
-#define ENB PB1 //9 
+#define In1 PB5 // 13
+#define In2 PB4 // 12
+#define In3 PD7 // 7
+#define In4 PD6 // 6
+#define ENA PB2 // 10
+#define ENB PB1 // 9
 
-
-int PotPrim = 510; // Potência do motor principal da curva
-int PotSec = 125;      // Potênica do motor secundário durante a curva
-int PotFrente = 255;   // Potência quando o robô estiver seguindo em frente
+int PotPrim = 510;   // Potência do motor principal da curva
+int PotSec = 125;    // Potênica do motor secundário durante a curva
+int PotFrente = 255; // Potência quando o robô estiver seguindo em frente
 
 // Sensores//
 #define s2 PC0
@@ -31,29 +30,28 @@ int PotFrente = 255;   // Potência quando o robô estiver seguindo em frente
 #define s7 PC5
 #define sp PD3
 
-
-//valor do Sensor lateral
-int valorsp = 0; 
+// valor do Sensor lateral
+int valorsp = 0;
 
 // PID
-float Kp = 0.6, Ki = 0.0, Kd = 0.0; // constante de Proporcionalidade, Integral e Diferencial
+float Kp = 2.5, Ki = 0., Kd = 2.5; // constante de Proporcionalidade, Integral e Diferencial
 int P = 0, I = 0, D = 0, PID = 0;
 double Setpoint = 0;
 int erro = 0, erro_anterior = 0;
 
 signed int media_p;
-int valor_max = 244;
-int valor_min = 203;
+int valor_max = 235;
+int valor_min = 212;
 
 char valor_parada = 0;
 char flag_stop = 0;
 
-
 unsigned char sensores[6];
 char buffer[10];
+int pesos[6] = {-300, -200, -100, 100, 200, 300};
 char tempo = 0;
 int PWM_esquerdo; // Potprim-pid
-int PWM_direito;      //Potprim+pid
+int PWM_direito;  // Potprim+pid
 // Declaração de funções
 
 void parada(void);
@@ -80,7 +78,7 @@ ISR(TIMER0_OVF_vect)
 int main()
 {
   setup();
-  
+
   adc_setup();
   tratar_leitura_do_ADC();
   TCCR0B = 0x03;   // defino o prescaler em 64
@@ -100,50 +98,43 @@ void setup(void)
 {
   // Motores
   DDRD = 0b11110110;
-  DDRB = 0b11111111; //Saídas 4,5,6,7 para motores, 1 para TX ; Entrada 3 para sensor lateral e Entrada 0 para RX 
+  DDRB = 0b11111111; // Saídas 4,5,6,7 para motores, 1 para TX ; Entrada 3 para sensor lateral e Entrada 0 para RX
   // Sensores
-  DDRC  = 0x00;
-
+  DDRC = 0x00;
 }
 
 void loop(void)
 {
- 
 
-  if(!flag_stop)
+  if (!flag_stop)
   {
-    
+
     parada();
   }
 }
-
 
 void MP(void)
 {
   leitura_sensores();
 
-  int p2 = -3000, p3 = -2000, p4 = -1000, p5 = 1000, p6 = 2000, p7 = 3000;
+  int denominador = 0;
+  int numerador = 0;
 
-  static int denominador = 0;
-  static int numerador = 0;
-
-  for(int i = 0; i < 6; i++)
+  for (int i = 0; i < 6; i++)
   {
     denominador += sensores[i];
   }
-  for (int i = 0; i < 6; i++)
-  {
-    numerador = ((sensores[0] * p2) + (sensores[1] * p3) + (sensores[2] * p4) + (sensores[3] * p5) + (sensores[4] * p6) + (sensores[5] * p7));
-  }
-  
-  if(!denominador)  media_p = 0;
+  numerador = ((sensores[0] * pesos[0]) + (sensores[1] * pesos[1]) + (sensores[2] * pesos[2]) + (sensores[3] * pesos[3]) + (sensores[4] * pesos[4]) + (sensores[5] * pesos[5]));
+
+  if (!denominador)
+    media_p = 0;
 
   else
   {
-    media_p =  ((numerador)/ (denominador));
+    media_p = (numerador) / (denominador);
   }
 
-  for(int i = 0; i < 6; i++)
+  for (int i = 0; i < 6; i++)
   {
     sprintf(buffer, "%d\t", sensores[i]);
     uart_string_sending_service(buffer);
@@ -155,12 +146,10 @@ void MP(void)
   controle_motor();
 }
 
-
 void timers1(void)
 {
 
   static unsigned int c_timer1 = 0;
- 
 
   if (c_timer1 < 10)
   {
@@ -172,7 +161,6 @@ void timers1(void)
     f_timer1();
     c_timer1 = 0;
   }
-
 }
 
 void f_timer1(void)
@@ -186,8 +174,6 @@ void calculo_erro(void)
   erro = Setpoint - media_p;
 
   calculoPID(erro);
-
- 
 }
 void calculoPID(int erro)
 {
@@ -210,13 +196,13 @@ void controle_motor(void)
   set_bit(PORTD, PD6);
   clear_bit(PORTD, PD7);
 
-if (PWM_esquerdo < 0)
+  if (PWM_esquerdo < 0)
   {
     set_bit(PORTB, PB5);
     clear_bit(PORTB, PB4);
     clear_bit(PORTD, PD6);
     set_bit(PORTD, PD7);
-}
+  }
   else if (PWM_direito < 0)
   {
     clear_bit(PORTB, PB5);
@@ -226,13 +212,12 @@ if (PWM_esquerdo < 0)
   }
   pwm_set_duty_service(PotPrim - PID, PWM_CHANNEL_1);
   pwm_set_duty_service(PotPrim + PID, PWM_CHANNEL_2);
-
 }
 void leitura_sensores(void)
 {
-  for(int i = 0; i < 6; i++)
+  for (int i = 0; i < 6; i++)
   {
-    if(AD_pins[i] > valor_max)
+    if (AD_pins[i] > valor_max)
     {
       sensores[i] = valor_max;
     }
@@ -248,9 +233,9 @@ void leitura_sensores(void)
   }
 }
 
-void parada (void)
+void parada(void)
 {
-  digitalRead (PD3);
+  digitalRead(PD3);
 
   if (valorsp < valor_max)
   {
@@ -259,12 +244,7 @@ void parada (void)
   else if (sensores[PD3] < valor_max && valor_parada >= 8)
   {
     valor_parada = 0;
-    delay(200);
+
     flag_stop = 1;
   }
 }
-
-
-
-
-// https://replit.com/ //
